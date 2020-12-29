156601d3b0450b65e0889acfb5e315d5827d20c6
Babajide Kale <bkale09@gmail.com>
2020-12-28 23:05:01 -0500

    added persistent state

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // DEV DEPENDENCIES
-import {connect, connet} from 'react-redux'
+import {connect} from 'react-redux'
 //ACTIONS
 import {addItem} from '../../redux/cart/cart.action'
 //Components
